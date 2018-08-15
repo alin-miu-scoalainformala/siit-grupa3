f3b2bb7c6f7b4299505314def56df6000ecbde89
vasilachi <vasilachi.razvan@gmail.com>
2018-08-15 14:17:50 +0300

    After last course

--- vasilachi-razvan/6-restaurant-menu-admin/scripts.js
+++ vasilachi-razvan/6-restaurant-menu-admin/scripts.js
@@ -1,15 +1,27 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA7E7-48yYINN54OsVWxjEdvFBYhFXnZ8U",
+    authDomain: "kfc-store.firebaseapp.com",
+    databaseURL: "https://kfc-store.firebaseio.com",
+    projectId: "kfc-store",
+    storageBucket: "kfc-store.appspot.com",
+    messagingSenderId: "425360991844"
+};
+firebase.initializeApp(config);
+
 var select = document.querySelector.bind(document);
+
 function getMenu() {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
-            console.log(JSON.parse(this.responseText));
+            //console.log(JSON.parse(this.responseText));
         }
     };
     xhttp.open("GET", "https://kfc-store.firebaseio.com/.json", true);
     xhttp.send();
 }
-getMenu();
+
 function submitForm(event){
     event.preventDefault();
     let newProduct = {
@@ -21,9 +33,33 @@ function submitForm(event){
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
-            console.log(JSON.parse(this.responseText));
+            // console.log(JSON.parse(this.responseText));
         }
     };
     xhttp.open("POST", `https://kfc-store.firebaseio.com/meniu/${product_category}/.json`, true);
     xhttp.send(JSON.stringify((newProduct)));
+}
+
+function uploadFile(event) {
+    event.preventDefault();
+    // Get file
+    var file = select("#image").files[0];
+
+    // Create a storage ref
+    var storageRef = firebase.storage().ref('images/' + file.name);
+
+    // Upload file
+    var task = storageRef.put(file);
+
+    task.on('state_changed',
+        function progress(snapshot){
+
+        },
+        function error(err) {
+
+        },
+        function complete() {
+            console.log(task.snapshot.downloadURL);
+        }
+    );
 }
