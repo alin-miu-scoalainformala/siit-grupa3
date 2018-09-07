cfedd22d9c2a0619cf1834614a6938fffb5b583f
CodrutCiulacu <ciulacu.codrut2002@gmail.con>
2018-09-07 21:47:33 +0300

    Update proiect final1

--- ciulacu-codrut/10-magazin-electronic/cart/cart.js
+++ ciulacu-codrut/10-magazin-electronic/cart/cart.js
@@ -1,16 +1,81 @@
-var transport = 0;
+const transport = 0;
 
-function getDataFromLocalStorgae(callback){
+function getProductsInTheCart(){
+    verifyIfCartIsNullOrEmpty();
+    var cart = JSON.parse(localStorage.getItem('cart'));
 
-    let list = JSON.parse(localStorage.getItem('cart'));
-
-    callback(list);
+    return cart;
 }
 
-function displayCart(list){
-    const main = document.getElementById('main');
+function displayCart(productsInCart){
+    initHTML(getProductsInTheCart());
+    productsInCart.forEach((product, index) => {
+        document.querySelector('tbody').innerHTML += generateHTMLForProducts(product, index);
+    });
+}
 
-    main.innerHTML = `<div class="card box-shadow" id="all">
+function  generateHTMLForProducts(product, index){
+    return `<tr>
+        <td style="width: 40%;"><a style="float: left;" href="../details/details.html?id=${product.key}">${product.nume}</a></td>
+        <td style="width: 15%;"><span style="float: center;">${product.pret}</span></td>
+        <td style="width: 15%;"><a style="float: left; text-decoration: none;" href="javascript:addCantity(${index});">+</a><span style="float: center;">${product.cantitate}</span><a style="float: right; text-decoration: none;" href="javascript:subtractCantity(${index});">-</a></td>
+        <td style="width: 15%;">${calcSubtotal(index)}</td>
+        <td style="width: 15%;"><a href="javascript:removeItem('${index}')">Remove</a></td>             
+    </tr> `;
+}
+
+function removeItem(key){
+    let list = getProductsInTheCart();
+
+    list.splice(key, 1);
+
+    localStorage.cart = JSON.stringify(list);
+
+    displayCart(list);
+}
+
+function addCantity(key){
+    let list = getProductsInTheCart();
+
+    if(list[key].cantitate < list[key].inStoc)
+        list[key].cantitate++;
+
+    localStorage.cart = JSON.stringify(list);
+
+    displayCart(list);
+}
+
+function subtractCantity(key){
+    let list = getProductsInTheCart();
+
+    if(list[key].cantitate > 1)
+        list[key].cantitate--;
+
+    localStorage.cart = JSON.stringify(list);
+
+    displayCart(list);
+}
+
+function calcSubtotal(key){
+    let list = getProductsInTheCart();
+    let elem = list[key];
+
+    let res = elem.pret * elem.cantitate;
+
+    return res;
+}
+
+function calcTotal(list){
+    let res = 0;
+    list.forEach((element, index) => {
+        res += calcSubtotal(index);
+    });
+
+    return res + transport;
+}
+
+function initHTML(list){
+    document.querySelector('#main').innerHTML = `<div class="card box-shadow" id="all">
                         <div class="row card-body">
                             <h5 class="card-title ml-3">Shopping Cart</h5>
                         </div>
@@ -35,72 +100,5 @@ function displayCart(list){
                                 <button class="btn btn-success">Buy Now</button> 
                             </div>
                     </div>`;
-
-    const body = document.querySelector('tbody');
-    let txt = '';
-    for (let i = 0; i < list.length; i++) {
-        if(list[i] != null && list[i] != undefined) {
-            txt += `<tr>
-                        <td style="width: 40%;"><a style="float: left;" href="edit.html?id=${list[i].key}">${list[i].nume}</a></td>
-                        <td style="width: 15%;"><span style="float: center;">${list[i].pret}</span></td>
-                        <td style="width: 15%;"><a style="float: left; text-decoration: none;" href="javascript:addCantity(${i});">+</a><span style="float: center;">${list[i].cantitate}</span><a style="float: right; text-decoration: none;" href="javascript:subtractCantity(${i});">-</a></td>
-                        <td style="width: 15%;">${calcSubtotal(i)}</td>
-                        <td style="width: 15%;"><a href="javascript:removeItem('${i}')">Remove</a></td>             
-                    </tr> `;
-        }
-    }
-
-
-    body.innerHTML = txt;
-
 }
 
-function removeItem(key){
-
-    let list = JSON.parse(localStorage.getItem('cart'));
-
-    list.splice(key, 1);
-
-    localStorage.cart = JSON.stringify(list);
-
-    getDataFromLocalStorgae(displayCart);
-}
-
-function addCantity(key){
-    let list = JSON.parse(localStorage.getItem('cart'));
-
-    list[key].cantitate++;
-
-    localStorage.cart = JSON.stringify(list);
-
-    getDataFromLocalStorgae(displayCart);
-}
-
-function subtractCantity(key){
-    let list = JSON.parse(localStorage.getItem('cart'));
-
-    if(list[key].cantitate > 1)
-        list[key].cantitate--;
-
-    localStorage.cart = JSON.stringify(list);
-
-    getDataFromLocalStorgae(displayCart);
-}
-
-function calcSubtotal(key){
-    let list = JSON.parse(localStorage.getItem('cart'));
-    let elem = list[key];
-
-    let res = elem.pret * elem.cantitate;
-
-    return res;
-}
-
-function calcTotal(list){
-    let res = 0;
-    list.forEach((element, index) => {
-        res += calcSubtotal(index);
-    });
-
-    return res + transport;
-}
--- ciulacu-codrut/10-magazin-electronic/details/details.js
+++ ciulacu-codrut/10-magazin-electronic/details/details.js
@@ -1,71 +1,57 @@
-var cartList = [];
-var productData;
+document.querySelector('.alert').style = "visibility: hidden;";
 
-function getDataOfObjectFromServer(idProdus){
-    let main = document.querySelector('#main');
-
-    let promise = fetch(`https://siit-buc3.firebaseio.com/onlineShop/products/${idProdus}.json`)
-        .then(res => {
-            main.innerHTML = `
-                    <img class="" src="https://i.redd.it/ounq1mw5kdxy.gif" style="" alt="Card image cap">
-            `;
-
-            return res.json();
-        });
-
-    promise
-        .then(res => {
-            setTimeout(() => {
-                productData = res;
-                displayDescription(res);
-            }, 500);
-
-        })
-        .catch(err => console.log(err));
-
+async function getDetailsAboutProduct(idProduct){
+    var details = await http.GET(PRODUCT_URI + idProduct + '.json');
+    return details;
 }
 
-
-function displayDescription(obj){
-    let main = document.querySelector('#main');
-
-    main.innerHTML = `<div class="card text-center mb-4 box-shadow">
-                            <img class="card-img-top pt-3 " id="img" src="" style="width: 400px; height: 287.992px; margin-left: 30%;" alt="Card image cap">
-                            <div class="card-body ml-5" style="float: right;">
-                                <h5 class="card-title mb-4" id="title"></h5>
-                                <h6 class="card-subtitle" id="price"></h6><br/>
-                                <h6 class="card-subtitle" id="desc"></h6><br/>
-                                <button class="btn btn-primary my-2 my-sm-0" onclick="storeCartInLocalStorage();">Add to cart</button>
-                            </div>
-                      </div>
-                    `
-
-    main.className = 'jumbotron container';
-
-    document.getElementById('img').src = obj.img;
-    document.getElementById('title').innerHTML = obj.title;
-    document.getElementById('price').innerHTML = parsePrice(obj.price);
-    document.getElementById('desc').innerHTML = obj.descriere;
-}
-
-function storeCartInLocalStorage(){
-    if(localStorage.getItem('cart') === null){
-        localStorage.setItem('cart', JSON.stringify([]));
-    }
+async function displayDetails(detailsAsPromise){
+    var details = await detailsAsPromise;
+    setTimeout(() => {
+        if(details != null){
+            initHTML(details);
+            displayDataAboutElement(details);
+        }
+    }, 500);
     
-    let cartList = JSON.parse(localStorage.getItem('cart'));
+}
+
+function initHTML(details){
+    console.log(details)
+    document.querySelector('#main').innerHTML =  `
+        <div class="card text-center mb-4 box-shadow">
+            <img class="card-img-top pt-3 " id="img" src="" style="width: 400px; height: 287.992px; margin-left: 30%;" alt="Card image cap">
+            <div class="card-body ml-5" style="float: right;">
+                <h5 class="card-title mb-4" id="title"></h5>
+                <h6 class="card-subtitle" id="price"></h6><br/>
+                <h6 class="card-subtitle" id="desc"></h6><br/>
+                <button class="btn btn-primary my-2 my-sm-0" onclick="putInYourCart();" data-toggle="alert">Add to cart</button>
+            </div>
+        </div>
+    `;
+    document.querySelector('#main').className = 'jumbotron container';
+}
+
+
+function displayDataAboutElement(details){
+    document.getElementById('img').src = details.img;
+    document.getElementById('title').innerHTML = details.title;
+    document.getElementById('price').innerHTML = parsePrice(details.price);
+    document.getElementById('desc').innerHTML = details.descriere;   
+}
+
+async function putInYourCart(){
+    document.querySelector('.alert').style = "visibility: visible;";
+    setTimeout(() => {
+        document.querySelector('.alert').style = "visibility: hidden;";
+    }, 3000);
+    var productData = await getDetailsAboutProduct(getProductIdFromURL());
+    var cartList = JSON.parse(localStorage.getItem('cart'));
     cartList.push({'nume': productData.title,
-                   'key': getURLVars()['id'],
+                   'key': getProductIdFromURL(),   
                    'cantitate': 1,
+                   'inStoc': productData.cantitate,
                    'pret': productData.price});
                    
-    localStorage.cart = JSON.stringify(cartList);
-}
-
-function getURLVars(){
-    let vars = [];
-    let parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-        vars[key] = value;
-    });
-    return vars;
+    localStorage.setItem('cart', JSON.stringify(cartList));
 }
